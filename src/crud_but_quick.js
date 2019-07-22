13d97e1b264d9cf001116719beefc83918e0cfba
Ivan Pantic <ivan@pantas.net>
2019-07-22 22:53:18 +0200

    Bootstrapping like pro

--- src/crud_but_quick.js
+++ src/crud_but_quick.js
@@ -1,6 +1,9 @@
+const libPath = require('path');
+
 const express = require('express');
 
 const { CBCOptions } = require('./types');
+const views = require('./views');
 
 /**
  * Create express.js router that will serve a CRUD ui
@@ -11,8 +14,10 @@ function crudButQuick(options) {
 
 	const router = express.Router();
 
+	router.use(express.static(libPath.resolve(__dirname, '../static')));
+
 	router.get('/', (req, res) => {
-		res.send('hooked up');
+		res.header('Content-Type', 'text/html').send(views.layout(options, '<h1>test</h1>'));
 	});
 
 	return router;
---
+++ src/views.js
@@ -0,0 +1,91 @@
+/**
+ * @param {CBCOptions} options
+ * @param content
+ */
+function layout(options, content) {
+	return `
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Hello world</title>
+
+		<link rel="stylesheet" href="/css/bootstrap.min.css" />
+		
+		<style type="text/css" rel="stylesheet">
+		</style>
+	</head>
+
+	<body>
+		
+		<nav class="navbar navbar-expand-md navbar-light bg-light">
+
+			<div class="container d-flex justify-content-between">
+				<a class="navbar-brand" href="#">Navbar</a>
+				<button class="navbar-toggler" type="button"
+						data-toggle="collapse" data-target="#navbarsExampleDefault"
+						aria-controls="navbarsExampleDefault" aria-expanded="false"
+						aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+					<ul class="navbar-nav mr-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Link</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled" href="#">Disabled</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#"
+									id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
+									aria-expanded="false">Dropdown</a>
+							<div class="dropdown-menu" aria-labelledby="dropdown01">
+								<a class="dropdown-item" href="#">Action</a>
+								<a class="dropdown-item" href="#">Another action</a>
+								<a class="dropdown-item" href="#">Something else here</a>
+							</div>
+						</li>
+					</ul>
+					<form class="form-inline my-2 my-lg-0">
+						<input class="form-control mr-sm-2" type="text"
+								placeholder="Search" aria-label="Search">
+						<button class="btn btn-outline-default my-2 my-sm-0"
+								type="submit">Search</button>
+					</form>
+				</div>
+			</div>
+
+		</nav>
+
+		<main role="main" class="container">
+			${content}
+		</main>
+		
+		<footer class="text-muted">
+      <div class="container">
+        <hr />
+        <p class="float-right">
+          <a href="#">Back to top</a>
+        </p>
+        <p>Copyright 2019, Company</p>
+      </div>
+    </footer>
+
+		<script src="/static/js/bootstrap.min.js"></script>
+	</body>
+</html>
+`;
+}
+
+module.exports = {
+	layout,
+};
